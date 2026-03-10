e149edf7fd3ad21a2e30589815ddb506362f0560
Aaron Zhang <AaronZhang@student.unsw.edu.au>
2026-03-10 15:25:47 +1100

    created stub functions for API route endpoints to match swagger docs

--- src/server.js
+++ src/server.js
@@ -11,6 +11,42 @@ app.use(express.json());
 // Serve Swagger UI
 const swaggerPath = path.resolve('../swagger.json');
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath))
+
+// Root route endpoint
+app.get('/', (req, res) => {
+  res.send('Invoice Generator API is running');
+});
+
+// Health Check route endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    service: 'Invoice Generator API'
+  });
+});
+
+app.post('/invoices', (req, res) => {
+  res.status(201).json({
+    message: "Invoice "
+  });
+});
+
+app.get('/invoices', (req, res) => {
+
+});
+
+app.get('/invoices/{invoiceId}', (req, res) => {
+
+});
+
+app.put('invoices/{invoiceId}', (req, res) => {
+
+});
+
+app.delete('invoices/{invoiceId}', (req, res) => {
+  
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 const PORT = 3000;
