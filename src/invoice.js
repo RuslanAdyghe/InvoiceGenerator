a5b26d86aca9f2155bd53e59c48e93f21627e963
Ruslan Stash <ruslan.stash06@gmail.com>
2026-03-11 19:40:37 +1100

    added supplier fragment

--- src/invoice.js
+++ src/invoice.js
@@ -24,3 +24,57 @@ function deliveryFragment(delivery) {
       up());
   }
 }
+
+function paymentMeansFragment(paymentMeans) {
+  const pfa = paymentMeans.PayeeFinancialAccount;
+
+  const frag = fragment()
+    .ele("cac:PaymentMeans")
+    .ele("cbc:PaymentMeansCode")
+    .txt(paymentMeans.PaymentMeansCode)
+    .up();
+
+  if (paymentMeans.PaymentDueDate) {
+    frag
+      .first()
+      .ele("cbc:PaymentDueDate")
+      .txt(paymentMeans.PaymentDueDate)
+      .up();
+  }
+
+  frag
+    .first()
+    .ele("cac:PayeeFinancialAccount")
+    .ele("cbc:ID")
+    .txt(pfa.ID)
+    .up()
+    .ele("cbc:Name")
+    .txt(pfa.Name)
+    .up()
+    .ele("cbc:CurrencyCode")
+    .att("currencyID", pfa.Currency)
+    .txt(pfa.Currency)
+    .up()
+    .up();
+
+  return frag;
+}
+
+function supplierFragment(supplier) {
+  const frag = fragment().ele("cac:AccountingSupplierParty").ele("cac:Party");
+
+  if (supplier.ID) {
+    frag.first().first().ele("cbc:EndpointID").txt(supplier.ID).up();
+  }
+
+  frag
+    .first()
+    .first()
+    .ele("cac:PartyName")
+    .ele("cbc:Name")
+    .txt(supplier.Name)
+    .up()
+    .up();
+
+  return frag;
+}
