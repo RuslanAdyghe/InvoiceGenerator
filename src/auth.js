2a8025fecdc4be37e4f80462b007a169801c47ae
Ruslan Stash <Ruslan.stash06@gmail.com>
2026-03-12 15:19:25 +1100

    added user database storing

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,7 @@
 import { PutCommand, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import db from "../db.js";
 
-export async function createUser(UserEmail, companyName) {
+async function createUser(UserEmail, companyName) {
   const id = crypto.randomUUID();
   await db.send(
     new PutCommand({
@@ -16,3 +16,27 @@ export async function createUser(UserEmail, companyName) {
   );
   return { id };
 }
+
+async function getUserById(id) {
+  const result = await db.send(
+    new GetCommand({
+      TableName: "users",
+      Key: { id },
+    }),
+  );
+  return result.Item;
+}
+
+async function getUserByEmail(email) {
+  const result = await db.send(
+    new QueryCommand({
+      TableName: "users",
+      IndexName: "email-index",
+      KeyConditionExpression: "email = :email",
+      ExpressionAttributeValues: { ":email": email },
+    }),
+  );
+  return result.Items?.[0];
+}
+
+export { createUser, getUserById, getUserByEmail };
