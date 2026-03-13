ad5d5d826f565f350e7cc511bc67470e0a4ae262
Aaron Zhang <z5476921@ad.unsw.edu.au>
2026-03-14 02:09:31 +1100

    made createInvoice page on frontend and connected it with API routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
+import CreateInvoice from "./pages/CreateInvoice"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-invoice" element={<CreateInvoice />} />
       </Routes>
     </BrowserRouter>  
   )
