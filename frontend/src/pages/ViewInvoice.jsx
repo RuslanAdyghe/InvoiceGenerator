276cb23ee9d9cec5957c119f8df2ee7bfb9dc8fe
Ruslan Stash <ruslan.stash06@gmail.com>
2026-04-05 21:46:34 +1000

    Update ViewInvoice.jsx

--- frontend/src/pages/ViewInvoice.jsx
+++ frontend/src/pages/ViewInvoice.jsx
@@ -19,7 +19,7 @@ function InvoiceDetail() {
       const token = localStorage.getItem("token");
       try {
         const response = await fetch(
-          `http://localhost:3000/invoices/${invoiceId}`,
+          `${import.meta.env.VITE_API_URL}/invoices/${invoiceId}`,
           {
             headers: { Authorization: `Bearer ${token}` },
           },
@@ -63,7 +63,7 @@ function InvoiceDetail() {
   const handleDownload = async () => {
     const token = localStorage.getItem("token");
     const response = await fetch(
-      `http://localhost:3000/invoices/${invoiceId}/download`,
+      `${import.meta.env.VITE_API_URL}/invoices/${invoiceId}/download`,
       {
         headers: { Authorization: `Bearer ${token}` },
       },
@@ -85,7 +85,7 @@ function InvoiceDetail() {
     const token = localStorage.getItem("token");
     try {
       const response = await fetch(
-        `http://localhost:3000/invoices/${invoiceId}`,
+        `${import.meta.env.VITE_API_URL}/invoices/${invoiceId}`,
         {
           method: "DELETE",
           headers: { Authorization: `Bearer ${token}` },
@@ -106,7 +106,7 @@ function InvoiceDetail() {
     const token = localStorage.getItem("token");
     try {
       const response = await fetch(
-        `http://localhost:3000/invoices/${invoiceId}/status`,
+        `${import.meta.env.VITE_API_URL}/invoices/${invoiceId}/status`,
         {
           method: "PUT",
           headers: {
